108838f00cd378c1c43cbc9342fe2313fcb1a1c3
Ftoro <fjtorov@ine.gob.cl>
2026-02-09 14:39:02 -0300

    render termino de version entregable

--- R/_load_all.R
+++ R/_load_all.R
@@ -3,6 +3,14 @@ library(janitor)
 library(htmltools)
 library(tibble)
 
+metadata_reporte <- list(
+  fecha_descarga   = as.Date("2026-01-26"),
+  fecha_generacion = Sys.Date()
+)
+
+fecha_ultima_actualizacion <- metadata_reporte$fecha_generacion
+
+
 source("R/report.R",  encoding = "UTF-8")
 source("R/target.R",  encoding = "UTF-8")
 source("R/grafic.R",  encoding = "UTF-8")
